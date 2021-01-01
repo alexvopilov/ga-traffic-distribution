42aadb5ba96fddb72cfec82f66ebebfa9d756e3d
Sasha <40319171+lonagi@users.noreply.github.com>
2021-01-01 17:59:54 +0200

    Trolleybus class

--- trafic.py
+++ trafic.py
@@ -8,3 +8,11 @@ class BKM321:
         self.seat_places = 26
         self.capacity = 115
         self.count = 101
+
+class Trolleybus:
+    def __init__(self,id,cars=0,people=0,model=BKM321):
+        self.id = id
+        self.car = model()
+        self.cars = cars
+        self.people = people
+
