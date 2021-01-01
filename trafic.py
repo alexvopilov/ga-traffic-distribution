9bd873a3b8c423c5e7b740a4ac3784636fbbe56b
Sasha <40319171+lonagi@users.noreply.github.com>
2021-01-01 17:59:38 +0200

    BKM321

---
+++ trafic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import pandas as pd
+class BKM321:
+    def __init__(self):
+        self.seat_places = 26
+        self.capacity = 115
+        self.count = 101
